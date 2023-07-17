ad8a6ccb1805aa32ae138b84d3a79956e594b510
DaylightNebula <daylightnebula1@gmail.com>
2023-07-16 21:37:07 -0600

    Fixed module loader

--- Meld-Server/src/main/kotlin/io/github/daylightnebula/meld/server/modules/ModuleLoader.kt
+++ Meld-Server/src/main/kotlin/io/github/daylightnebula/meld/server/modules/ModuleLoader.kt
@@ -3,52 +3,52 @@ package io.github.daylightnebula.meld.server.modules
 import org.json.JSONObject
 import java.io.File
 import java.net.URLClassLoader
+import java.util.jar.JarFile
 
 object ModuleLoader {
     val modules = mutableListOf<MeldModule>()
     private val modulesFolder = File("modules")
 
     fun load() {
-        // load all jar files
-        modulesFolder.listFiles()?.forEach {
-            // only jar files
-            if (it.extension != "jar") return@forEach
+        // get a list of all jar files to load
+        val jarFiles = modulesFolder.listFiles()!!
+            .filter { it.extension == "jar" }
 
-            // load the jar file
-            loadJar(it)
+        // create a loader
+        val loader = URLClassLoader(jarFiles.map { it.toURI().toURL() }.toTypedArray())
+
+        // initialize each module
+        for (jarFile in jarFiles) {
+            println("Loading module ${jarFile.name}...")
+            // get config file
+            val config = JarFile(jarFile).use {
+                val entry = it.getEntry("module.json")
+                JSONObject(it.getInputStream(entry).bufferedReader().readText())
+            }
+            val mainClass = config.optString("main")
+            if (mainClass == null) {
+                println("WARN failed to load module ${jarFile.name}")
+                return
+            }
+
+            // get main class and its constructor
+            val constructor = Class.forName(mainClass, true, loader).getDeclaredConstructor()
+            constructor.trySetAccessible()
+
+            // create new module
+            val module = constructor.newInstance() as? MeldModule
+            if (module == null) {
+                println("WARN failed to initialize module ${jarFile.name}")
+                return
+            }
+
+            // save
+            modules.add(module)
+
+            // enable the module
+            module.onEnable()
+            println("Loaded module ${jarFile.name}")
         }
-    }
-
-    private fun loadJar(jar: File) {
-        println("Loading module ${jar.name}...")
-        // get loader
-        val loader = URLClassLoader(arrayOf(jar.toURI().toURL()))
-
-        // get config file
-        val config = JSONObject(loader.getResource("module.json")?.readText() ?: "{}")
-        val mainClass = config.optString("main")
-        if (mainClass == null) {
-            println("WARN failed to load module ${jar.name}")
-            return
-        }
-
-        // get main class and its constructor
-        val constructor = Class.forName(mainClass, true, loader).getDeclaredConstructor()
-        constructor.trySetAccessible()
-
-        // create new module
-        val module = constructor.newInstance() as? MeldModule
-        if (module == null) {
-            println("WARN failed to initialize module ${jar.name}")
-            return
-        }
-
-        // save
-        modules.add(module)
-
-        // enable the module
-        module.onEnable()
-        println("Loaded module ${jar.name}")
     }
 }
 
