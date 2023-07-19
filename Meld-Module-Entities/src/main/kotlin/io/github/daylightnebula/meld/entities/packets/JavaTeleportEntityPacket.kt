eb510fad92cb1404e32b064ad88d02ee1da4d7ed
DaylightNebula <daylightnebula1@gmail.com>
2023-07-18 20:27:36 -0600

    Added java teleport entity packet

---
+++ Meld-Module-Entities/src/main/kotlin/io/github/daylightnebula/meld/entities/packets/JavaTeleportEntityPacket.kt
@@ -0,0 +1,27 @@
+package io.github.daylightnebula.meld.entities.packets
+
+import io.github.daylightnebula.meld.server.networking.common.AbstractReader
+import io.github.daylightnebula.meld.server.networking.common.ByteWriter
+import io.github.daylightnebula.meld.server.networking.java.JavaPacket
+import io.github.daylightnebula.meld.server.noDecode
+import org.cloudburstmc.math.vector.Vector2f
+import org.cloudburstmc.math.vector.Vector3f
+
+class JavaTeleportEntityPacket(
+    var entityID: Int = 0,
+    var position: Vector3f = Vector3f.ZERO,
+    var rotation: Vector2f = Vector2f.ZERO,
+    var onGround: Boolean = false
+): JavaPacket {
+    override val id: Int = 0x68
+    override fun decode(reader: AbstractReader) = noDecode()
+    override fun encode(writer: ByteWriter) {
+        writer.writeVarInt(entityID)
+        writer.writeDouble(position.x.toDouble())
+        writer.writeDouble(position.y.toDouble())
+        writer.writeDouble(position.z.toDouble())
+        writer.writeByte(rotation.x.toAngleByte())
+        writer.writeByte(rotation.y.toAngleByte())
+        writer.writeBoolean(onGround)
+    }
+}
