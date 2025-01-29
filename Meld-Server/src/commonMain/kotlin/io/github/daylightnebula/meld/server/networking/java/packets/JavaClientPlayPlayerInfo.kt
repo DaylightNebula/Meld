515fefa8068a19fa97cc0e697603238730e3d12f
DaylightNebula <daylightnebula1@gmail.com>
2025-01-29 09:29:50 -0800

    Finished JavaClientPlayPlayerInfo

--- Meld-Server/src/commonMain/kotlin/io/github/daylightnebula/meld/server/networking/java/packets/JavaClientPlayPlayerInfo.kt
+++ Meld-Server/src/commonMain/kotlin/io/github/daylightnebula/meld/server/networking/java/packets/JavaClientPlayPlayerInfo.kt
@@ -2,21 +2,12 @@ package io.github.daylightnebula.meld.server.networking.java.packets
 
 import io.github.daylightnebula.meld.ksp.data.IReader
 import io.github.daylightnebula.meld.ksp.data.RegisterPacket
-import io.github.daylightnebula.meld.server.AnonymousNBT
-import io.github.daylightnebula.meld.server.BoolCodec
-import io.github.daylightnebula.meld.server.LongCodec
-import io.github.daylightnebula.meld.server.StdByteArrayCodec
-import io.github.daylightnebula.meld.server.StringCodec
-import io.github.daylightnebula.meld.server.UuidCodec
-import io.github.daylightnebula.meld.server.VarIntCodec
+import io.github.daylightnebula.meld.server.*
 import io.github.daylightnebula.meld.server.networking.java.JavaConnectionState
 import io.github.daylightnebula.meld.server.networking.java.JavaPacket
 import net.benwoodworth.knbt.NbtTag
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
-
-// todo move decoders to creator sub-interface of Action
-// todo finish encode and decode functions in root
 
 @OptIn(ExperimentalUuidApi::class)
 @RegisterPacket("packet_player_info")
@@ -25,25 +16,74 @@ class JavaClientPlayPlayerInfo(
 ): JavaPacket {
     override val ID = 0x40
     override val STATE = JavaConnectionState.IN_GAME
-    override fun encode() = TODO()
+    override fun encode(): ByteArray {
+        // compile bit flags by looping through all actions in all data
+        var flags: UByte = 0u
+        for (data in data)
+            for (action in data.actions)
+                if (flags and action.mask == 0u.toUByte())
+                    flags = flags or action.mask
+
+        // compile final byte array by encoding every data
+        return UByteCodec.encode(flags) +
+                VarIntCodec.encode(data.size) +
+                data.map(Data::encode).fold(byteArrayOf()) { a, b -> a + b }
+    }
 
     companion object: JavaPacket.Creator<JavaClientPlayPlayerInfo> {
         override val ID = 0x40
         override val STATE = JavaConnectionState.IN_GAME
-        override fun decode(reader: IReader) = TODO()
+        override fun decode(reader: IReader): JavaClientPlayPlayerInfo {
+            val flags = UByteCodec.decode(reader)
+            val numEntries = VarIntCodec.decode(reader)
+            return JavaClientPlayPlayerInfo(
+                data = (0 until numEntries).map { Data.decode(reader, flags) }
+            )
+        }
     }
 
     // Define data for each user to be modified
     class Data(
         val uuid: Uuid,
         val actions: List<Action>
-    )
+    ) {
+        fun encode() = UuidCodec.encode(uuid) + actions.map(Action::encode).fold(byteArrayOf()) { a, b -> a + b }
+
+        companion object {
+            fun decode(reader: IReader, flags: UByte): Data {
+                // setup
+                val uuid = UuidCodec.decode(reader)
+                val actions = mutableListOf<Action>()
+
+                // decode actions
+                AddPlayerAction.checkedDecode(actions, reader, flags)
+                InitializeChatAction.checkedDecode(actions, reader, flags)
+                UpdateGameModeAction.checkedDecode(actions, reader, flags)
+                UpdateListedAction.checkedDecode(actions, reader, flags)
+                UpdateLatencyAction.checkedDecode(actions, reader, flags)
+                UpdateDisplayNameAction.checkedDecode(actions, reader, flags)
+                UpdatePriorityAction.checkedDecode(actions, reader, flags)
+                UpdateHatVisibilityAction.checkedDecode(actions, reader, flags)
+
+                // build final data
+                return Data(uuid, actions)
+            }
+        }
+    }
 
     // Interface for each action that may be applied to each player
     interface Action {
-        val mask: Int
+        val mask: UByte
         fun encode(): ByteArray
+    }
+
+    interface ActionCreator {
+        val mask: UByte
         fun decode(reader: IReader): Action
+
+        fun checkedDecode(actions: MutableList<Action>, reader: IReader, flags: UByte): Boolean =
+            if (flags and mask > 0u) actions.add(decode(reader))
+            else false
     }
 
     // Defines an action where a player is added to the server
@@ -51,7 +91,9 @@ class JavaClientPlayPlayerInfo(
         val name: String,
         val properties: List<Property>
     ): Action {
-        override val mask: Int = 0x01
+        data class Property(val name: String, val value: String, val signature: String? = null)
+
+        override val mask: UByte = 1u
         override fun encode() =
             StringCodec.encode(name) +
             VarIntCodec.encode(properties.size) +
@@ -61,19 +103,19 @@ class JavaClientPlayPlayerInfo(
                 if (it.signature != null) byteArrayOf(0x01) + StringCodec.encode(it.signature) else byteArrayOf(0x00)
             }.fold(byteArrayOf()) { acc, bytes -> acc + bytes }
 
-        override fun decode(reader: IReader) = AddPlayerAction(
-            name = StringCodec.decode(reader),
-            properties = (0 until VarIntCodec.decode(reader)).map {
-                Property(
-                    name = StringCodec.decode(reader),
-                    value = StringCodec.decode(reader),
-                    signature = if (reader.read() > 0) StringCodec.decode(reader) else null
-                )
-            }
-        )
-
-        // Define a player property
-        data class Property(val name: String, val value: String, val signature: String? = null)
+        companion object: ActionCreator {
+            override val mask: UByte = 1u
+            override fun decode(reader: IReader) = AddPlayerAction(
+                name = StringCodec.decode(reader),
+                properties = (0 until VarIntCodec.decode(reader)).map {
+                    Property(
+                        name = StringCodec.decode(reader),
+                        value = StringCodec.decode(reader),
+                        signature = if (reader.read() > 0) StringCodec.decode(reader) else null
+                    )
+                }
+            )
+        }
     }
 
     // Defines the chat initialization action
@@ -83,55 +125,83 @@ class JavaClientPlayPlayerInfo(
         val encodedPublicKey: ByteArray,
         val publicKeySignature: ByteArray
     ): Action {
-        override val mask: Int = 0x02
+        override val mask: UByte = 2u
 
         override fun encode() = UuidCodec.encode(sessionID) + LongCodec.encode(publicKey) +
                 StdByteArrayCodec.encode(encodedPublicKey) +
                 StdByteArrayCodec.encode(publicKeySignature)
 
-        override fun decode(reader: IReader) = InitializeChatAction(
-            sessionID = UuidCodec.decode(reader),
-            publicKey = LongCodec.decode(reader),
-            encodedPublicKey = StdByteArrayCodec.decode(reader),
-            publicKeySignature = StdByteArrayCodec.decode(reader)
-        )
+        companion object: ActionCreator {
+            override val mask: UByte = 2u
+
+            override fun decode(reader: IReader) = InitializeChatAction(
+                sessionID = UuidCodec.decode(reader),
+                publicKey = LongCodec.decode(reader),
+                encodedPublicKey = StdByteArrayCodec.decode(reader),
+                publicKeySignature = StdByteArrayCodec.decode(reader)
+            )
+        }
     }
 
     // Defines the game mode of the defined player
     class UpdateGameModeAction(val gameMode: Int): Action {
-        override val mask: Int = 0x04
+        override val mask: UByte = 4u
         override fun encode() = VarIntCodec.encode(gameMode)
-        override fun decode(reader: IReader) = UpdateGameModeAction(VarIntCodec.decode(reader))
+
+        companion object: ActionCreator {
+            override val mask: UByte = 4u
+            override fun decode(reader: IReader) = UpdateGameModeAction(VarIntCodec.decode(reader))
+        }
     }
 
     // Define whether a player is listed on the tablist
     class UpdateListedAction(val listed: Boolean): Action {
-        override val mask: Int = 0x08
+        override val mask: UByte = 8u
         override fun encode() = BoolCodec.encode(listed)
-        override fun decode(reader: IReader) = UpdateListedAction(BoolCodec.decode(reader))
+
+        companion object: ActionCreator {
+            override val mask: UByte = 8u
+            override fun decode(reader: IReader) = UpdateListedAction(BoolCodec.decode(reader))
+        }
     }
 
-    class PingAction(val ping: Int): Action {
-        override val mask: Int = 0x10
+    class UpdateLatencyAction(val ping: Int): Action {
+        override val mask: UByte = 16u
         override fun encode() = VarIntCodec.encode(ping)
-        override fun decode(reader: IReader) = PingAction(VarIntCodec.decode(reader))
+
+        companion object: ActionCreator {
+            override val mask: UByte = 16u
+            override fun decode(reader: IReader) = UpdateLatencyAction(VarIntCodec.decode(reader))
+        }
     }
 
-    class DisplayNameAction(val displayName: NbtTag): Action {
-        override val mask: Int = 0x20
+    class UpdateDisplayNameAction(val displayName: NbtTag): Action {
+        override val mask: UByte = 32u
         override fun encode() = AnonymousNBT.encode(displayName)
-        override fun decode(reader: IReader) = DisplayNameAction(AnonymousNBT.decode(reader))
+
+        companion object: ActionCreator {
+            override val mask: UByte = 32u
+            override fun decode(reader: IReader) = UpdateDisplayNameAction(AnonymousNBT.decode(reader))
+        }
     }
 
-    class PriorityAction(val priority: Int): Action {
-        override val mask: Int = 0x40
+    class UpdatePriorityAction(val priority: Int): Action {
+        override val mask: UByte = 64u
         override fun encode() = VarIntCodec.encode(priority)
-        override fun decode(reader: IReader) = PriorityAction(VarIntCodec.decode(reader))
+
+        companion object: ActionCreator {
+            override val mask: UByte = 64u
+            override fun decode(reader: IReader) = UpdatePriorityAction(VarIntCodec.decode(reader))
+        }
     }
 
-    class VisibleAction(val visible: Boolean): Action {
-        override val mask: Int = 0x80
+    class UpdateHatVisibilityAction(val visible: Boolean): Action {
+        override val mask: UByte = 128u
         override fun encode() = BoolCodec.encode(visible)
-        override fun decode(reader: IReader) = VisibleAction(BoolCodec.decode(reader))
+
+        companion object: ActionCreator {
+            override val mask: UByte = 128u
+            override fun decode(reader: IReader) = UpdateHatVisibilityAction(BoolCodec.decode(reader))
+        }
     }
 }
